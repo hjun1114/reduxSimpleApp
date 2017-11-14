65ea4248dd8f73e1881f614ccc734fba55bcba5f
Brandon Cho <hjun1114@gmail.com>
2017-11-14 00:27:26 -0500

    adding reducer and a list component

---
+++ src/reducers/reducer_exampleCharacters.js
@@ -0,0 +1,10 @@
+export default function() {
+  return [
+    {title: "Dwight Schrute"},
+    {title: "Jim Halpert"},
+    {title: "Pam Halpert"},
+    {title: "Michael Scott"}
+  ]
+}
+// reducer is a function that returns a piece of application state
+// note that the application can have many pieces of states. => many different reducers
